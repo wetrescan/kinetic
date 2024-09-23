6ce3a317b63e0b943f8dc6f97c2a27af02a30c31
Wecan <sdb@dibamaro.eu>
2024-09-23 16:02:11 +0200

    cambiosnuevos

--- src/main/java/net/wecan/kinetic/Kinetic.java
+++ src/main/java/net/wecan/kinetic/Kinetic.java
@@ -1,9 +1,22 @@
 package net.wecan.kinetic;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Supplier;
+
+import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
+import static com.mojang.brigadier.builder.RequiredArgumentBuilder.argument;
 
 public class Kinetic implements ModInitializer {
 	public static final String MOD_ID = "kinetic";
@@ -20,5 +33,76 @@ public class Kinetic implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("kkkkinetic");
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(literal("maincommand")
+					.then(literal("firstsubcommand")
+							.executes(context -> {
+								context.getSource().getClass(Text.literal("Executed /maincommand firstsubcommand!"), false);
+								return Command.SINGLE_SUCCESS;
+							})
+							.then(literal("firstsubsubcommand")
+									.executes(context -> {
+										context.getSource().getClass(Text.literal("Executed /maincommand firstsubcommand firstsubsubcommand!"), false);
+										return Command.SINGLE_SUCCESS;
+									})
+							)
+					)
+					.then(literal("secondsubcommand")
+							.then(argument("number", IntegerArgumentType.integer())
+									.executes(context -> {
+										int number = IntegerArgumentType.getInteger(context, "number");
+										context.getSource().getClass(Text.literal("Executed /maincommand secondsubcommand with number: " + number), false);
+										return Command.SINGLE_SUCCESS;
+									})
+									.then(literal("secondsubsubcommand")
+											.executes(context -> {
+												int number = IntegerArgumentType.getInteger(context, "number");
+												context.getSource().getClass(Text.literal("Executed /maincommand secondsubcommand secondsubsubcommand with number: " + number), false);
+												return Command.SINGLE_SUCCESS;
+											})
+									)
+							)
+					)
+			);
+		});
+	}
+
+
+
+	private int executeDirection(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback((Supplier<Text>) Text.literal("Executed Direction"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeSouth(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback((Supplier<Text>) Text.literal("Executed south"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeNorth(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback((Supplier<Text>) Text.literal("Executed north"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeEast(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback((Supplier<Text>) Text.literal("Executed East"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeWest(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback((Supplier<Text>) Text.literal("Executed west"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeValue(CommandContext<ServerCommandSource> context) {
+		context.getSource().sendFeedback(Text.literal("Executed value"), false);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private int executeNumber(CommandContext<ServerCommandSource> context) {
+		// Get the number argument
+		int number = IntegerArgumentType.getInteger(context, "number");
+		context.getSource().sendFeedback(Text.literal("Executed number with number " + number), false);
+		return Command.SINGLE_SUCCESS;
 	}
 }
